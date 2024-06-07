8c9f82db8aa0627a2621486db1fab69aefa398fe
wynMatte <swlapiz@gmail.com>
2024-06-07 15:29:39 +0800

    implemented watchdog for detection on tracks.json

--- main.py
+++ main.py
@@ -58,3 +58,42 @@ with open("ammp/assets/tracks.json", "w") as output:
     output.write(json)
 
 '''TODO: JSON TO MYSQL'''
+import pandas
+import sqlalchemy
+
+# Watchdog file change detection of tracks.json
+import watchdog.events
+import watchdog.observers
+import time
+
+
+class Handler(watchdog.events.PatternMatchingEventHandler):
+	def __init__(self):
+		# Set the patterns for PatternMatchingEventHandler
+		watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.json'],
+															ignore_directories=True, case_sensitive=False)
+
+	def on_created(self, event):
+		print("Watchdog received created event - % s." % event.src_path)
+		# Event is created, you can process it now
+
+	def on_modified(self, event):
+		print("Watchdog received modified event - % s." % event.src_path)
+		# Event is modified, you can process it now
+
+
+if __name__ == "__main__":
+	src_path = "ammp/assets"
+	event_handler = Handler()
+	observer = watchdog.observers.Observer()
+	observer.schedule(event_handler, path=src_path, recursive=True)
+	observer.start()
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		observer.stop()
+	observer.join()
+
+
+# 
